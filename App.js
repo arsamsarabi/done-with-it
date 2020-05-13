a11a174b439d4f8f2a5bdd64b13d061907b29951
Arsam Sarabi <arsamsarabi@me.com>
2020-05-13 16:56:45 +0100

    ➕ Adds react-native-community hooks

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import React from 'react'
-import { StyleSheet, Text, SafeAreaView, Image } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from 'react-native'
 
 export default App = () => {
   return (
     <SafeAreaView style={styles.container}>
+      <Image
+        source={require('./assets/icon.png')}
+        blurRadius={0}
+        fadeDuration={1000}
+        style={styles.logo}
+      />
+
       <Text>Done With It ♻️</Text>
     </SafeAreaView>
   )
@@ -13,7 +30,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f1f1f1',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight + 10 : 0,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
   },
 })
