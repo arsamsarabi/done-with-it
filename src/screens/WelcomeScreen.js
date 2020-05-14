a79b1a0268425da86bb30c1169a1e06fa3be8d26
Arsam Sarabi <arsamsarabi@me.com>
2020-05-14 10:26:39 +0100

    🎨 <WelcomeScreen /> button placeholders

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import { StyleSheet, Text, ImageBackground, Image, StatusBar, Platform } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, Image, StatusBar, Platform } from 'react-native'
 
 import { Palette } from '../utils'
 
 export const WelcomeScreen = () => {
   return (
     <ImageBackground style={styles.container} source={require('../../assets/images/bedroom.jpg')}>
-      <Image
-        source={require('../../assets/icon.png')}
-        blurRadius={0}
-        fadeDuration={500}
-        style={styles.logo}
-      />
-
-      <Text>Sell What You Don't Need</Text>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../../assets/icon.png')}
+          blurRadius={0}
+          fadeDuration={500}
+          style={styles.logo}
+        />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.placeholderRed} />
+      <View style={styles.placeholderGreen} />
     </ImageBackground>
   )
 }
@@ -24,10 +27,23 @@ const styles = StyleSheet.create({
     backgroundColor: Palette.white,
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight + 10 : 35,
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'flex-end',
+  },
+  logoContainer: {
+    flex: 1,
   },
   logo: {
     width: 150,
     height: 150,
+  },
+  placeholderGreen: {
+    width: '100%',
+    height: 75,
+    backgroundColor: Palette.green,
+  },
+  placeholderRed: {
+    width: '100%',
+    height: 75,
+    backgroundColor: Palette.red,
   },
 })
--- src/utils/palette.js
+++ src/utils/palette.js
@@ -1,6 +1,6 @@
 export const Palette = {
   white: '#F1F1F1',
   black: '#333333',
-  green: '#16a085',
+  green: '#4ECDC4',
   red: '#fc5c65',
 }
