9c4727ec6531500add0cd87dcbc0d75f56f3389c
Arsam Sarabi <arsamsarabi@me.com>
2020-05-15 13:18:47 +0100

    🚧 WIP Picker modal

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, TouchableWithoutFeedback, View, Modal, Button } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import { palette, defaultStyles } from '../config'
 import { AppText } from './AppText'
+import { Screen } from './Screen'
 
 export const AppPicker = ({ icon, placeholder, ...rest }) => {
+  const [modalVisibility, setModalVisibility] = useState(false)
   return (
-    <View style={styles.container}>
-      {icon && <MaterialCommunityIcons name={icon} size={24} color={palette.text} />}
-      <AppText style={styles.placeholder}>{placeholder}</AppText>
-      <MaterialCommunityIcons name="chevron-down" size={20} color={palette.text} />
-    </View>
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisibility(true)}>
+        <View style={styles.container}>
+          {icon && <MaterialCommunityIcons name={icon} size={24} color={palette.text} />}
+          <AppText style={styles.placeholder}>{placeholder}</AppText>
+          <MaterialCommunityIcons name="chevron-down" size={20} color={palette.text} />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisibility} animationType="slide">
+        <Screen>
+          <Button title="close" onPress={() => setModalVisibility(false)} />
+        </Screen>
+      </Modal>
+    </>
   )
 }
 
